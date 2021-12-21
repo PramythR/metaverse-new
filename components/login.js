40027d32c948a64f9b96beee8e2e0cf5362f3299
pramithrajapaksa <pramithrajapaksha69@gmail.com>
2021-12-21 10:33:52 +0530

    login implement

--- components/login.js
+++ components/login.js
@@ -16,7 +16,7 @@ function Login(props) {
               width={200}
               height={200}
               />
-               <button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"> Login to the METAVERSE</button>
+               <button onClick={authenticate} className="bg-green-500 rounded-lg p-5 font-bold animate-pulse"> Login to the METAVERSE</button>
            </div>
            <div className="w-full h-screen">
                  <Image src="https://links.papareact.com/55n" layout="fill" objectFit="cover"/>
